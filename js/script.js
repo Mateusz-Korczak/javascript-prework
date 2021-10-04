346486e0b6621e82094877fa3237e818a37e01e6
Mateusz Korczak <korczak.programowanie@gmail.com>
2021-10-04 14:55:42 +0200

    Task 3 result

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
 
-//Declarations:
 const choice = ['kamień', 'papier', 'nożyce']
 let playerMove;
 let computerMove;
 
 
-//Display funtions:
 function printMessage(msg){
 	let div = document.createElement('div');
 	div.innerHTML = msg;
@@ -16,22 +14,24 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
-
-// Computer move
 function getRandomInt(min, max) {
 	min = Math.ceil(min);
 	max = Math.floor(max);
 	return Math.floor(Math.random() * (max - min)) + min;
-  }
+}
 
-computerMove = choice[getRandomInt(0,3)];
+function getMoveName(choiceIndex) {
+	return choice[choiceIndex];
+}
 
 
-// Player move
+computerMove = getMoveName(getRandomInt(0,3));
+
+
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 if (playerInput == 1 || playerInput == 2 || playerInput == 3) {
-	playerMove = choice[(playerInput-1)]
+	playerMove = getMoveName(playerInput-1);
 }
 else {
 	alert('Wprowadziłeś niepoprawną wartość');
@@ -39,19 +39,21 @@ else {
 }
 
 
-// Result score
-printMessage(`Ruch komputera: ${computerMove}`);
-printMessage(`Ruch gracza: ${playerMove}`);
+function displayResult(computerMove, playerMove){
+	printMessage(`Ruch komputera: ${computerMove}`);
+	printMessage(`Ruch gracza: ${playerMove}`);
+	
+	if ( playerMove === 'kamień' && computerMove === 'nożyce' || playerMove === 'papier' && computerMove === 'kamień' || playerMove === 'nożyce' && computerMove === 'papier')
+	{
+		printMessage('Wygrałeś!');
+	}
+	else if ( computerMove === 'kamień' && playerMove === 'nożyce' || computerMove === 'papier' && playerMove === 'kamień' || computerMove ==='nożyce' && playerMove === 'papier')
+	{
+		printMessage('Przegrałeś!');
+	}
+	else {
+		printMessage('Remis!');
+	}
+}
 
-
-if ( playerMove === 'kamień' && computerMove === 'nożyce' || playerMove === 'papier' && computerMove === 'kamień' || playerMove === 'nożyce' && computerMove === 'papier')
-{
-	printMessage('Wygrałeś!');
-}
-else if ( computerMove === 'kamień' && playerMove === 'nożyce' || computerMove === 'papier' && playerMove === 'kamień' || computerMove ==='nożyce' && playerMove === 'papier')
-{
-	printMessage('Przegrałeś!');
-}
-else {
-	printMessage('Remis!');
-}
+displayResult(computerMove, playerMove);
