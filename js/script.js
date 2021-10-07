d8f4a3255a693bdeb41b2d4b85a06cfabcb7cf92
Mateusz Korczak <korczak.programowanie@gmail.com>
2021-10-07 15:17:04 +0200

    corrected task with code refactore

--- js/script.js
+++ js/script.js
@@ -1,47 +1,42 @@
-function playGame(playerMove){
+{
+	// [DONE] cały kod znajdujący się w tym pliku zamknij w bloku za pomocą nawiasów klamrowych { }, aby unikać używania zmiennych/stałych globalnych,
+	// [DONE] jeśli to możliwe, używaj stałych,
+	// deklaracje stałych/zmiennych powinny być – w miarę możliwości – zagnieżdżone tak samo, jak miejsce ich wykorzystania, czyli staramy się używać możliwie małych zakresów,
+	// [DONE] unikaj sytuacji, w których tuż pod deklaracją zmiennej nadajesz jej wartość – lepiej zrobić to od razu w deklaracji,
+	// [DONE] zamień funkcje nazwane na funkcje anonimowe zapisane w stałych,
+	const playGame = function (playerMove){
 
-	const choice = ['kamień', 'papier', 'nożyce']
-	let computerMove;
+		const choice = ['kamień', 'papier', 'nożyce']
+		let computerMove = choice[(Math.floor(Math.random() * (3 - 0)) + 0)];
 
-	clearMessages();
+		clearMessages();
 
-	function getRandomInt(min, max) {
-		min = Math.ceil(min);
-		max = Math.floor(max);
-		return Math.floor(Math.random() * (max - min)) + min;
-	}
-
-	function getMoveName(choiceIndex) {
-		return choice[choiceIndex];
-	}
-
-	computerMove = getMoveName(getRandomInt(0,3));
-
-
-	function displayResult(computerMove, playerMove){
-		printMessage(`Ruch komputera: ${computerMove}`);
-		printMessage(`Ruch gracza: ${playerMove}`);
-		
-		if ( playerMove === 'kamień' && computerMove === 'nożyce' || playerMove === 'papier' && computerMove === 'kamień' || playerMove === 'nożyce' && computerMove === 'papier')
-		{
-			printMessage('Wygrałeś!');
+		const displayResult = function (computerMove, playerMove){
+			printMessage(`Ruch komputera: ${computerMove}`);
+			printMessage(`Ruch gracza: ${playerMove}`);
+			
+			if ( playerMove === 'kamień' && computerMove === 'nożyce' || playerMove === 'papier' && computerMove === 'kamień' || playerMove === 'nożyce' && computerMove === 'papier')
+			{
+				printMessage('Wygrałeś!');
+			}
+			else if ( computerMove === 'kamień' && playerMove === 'nożyce' || computerMove === 'papier' && playerMove === 'kamień' || computerMove ==='nożyce' && playerMove === 'papier')
+			{
+				printMessage('Przegrałeś!');
+			}
+			else {
+				printMessage('Remis!');
+			}
 		}
-		else if ( computerMove === 'kamień' && playerMove === 'nożyce' || computerMove === 'papier' && playerMove === 'kamień' || computerMove ==='nożyce' && playerMove === 'papier')
-		{
-			printMessage('Przegrałeś!');
-		}
-		else {
-			printMessage('Remis!');
-		}
+	
+		displayResult(computerMove, playerMove);
 	}
-
-	displayResult(computerMove, playerMove);
+	
+	  const rockButton = document.getElementById('kamień');
+	  const paperButton = document.getElementById('papier');
+	  const scissorsButton = document.getElementById('nożyce');
+	  
+	  rockButton.addEventListener('click', () => {playGame("kamień")});
+	  paperButton.addEventListener('click', () => {playGame("papier")});
+	  scissorsButton.addEventListener('click', () => {playGame("nożyce")});
 }
 
-  const rockButton = document.getElementById('kamień');
-  const paperButton = document.getElementById('papier');
-  const scissorsButton = document.getElementById('nożyce');
-  
-  rockButton.addEventListener('click', () => {playGame("kamień")});
-  paperButton.addEventListener('click', () => {playGame("papier")});
-  scissorsButton.addEventListener('click', () => {playGame("nożyce")});
